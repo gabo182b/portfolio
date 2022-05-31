3afa9ac19b4f668a9c07d6d06f96f74c8c860609
Gabriel <flexflit18@gmail.com>
2022-05-31 08:15:49 -0400

    portfolio section restructured

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Project } from "../Project";
+import { projectOne } from "../Project/data";
 import "./portfolio.scss";
 
 const Portfolio = () => {
@@ -6,14 +8,7 @@ const Portfolio = () => {
     <section className="portfolio-section" id="portfolio">
       <h2 className="portfolio-section__title">Projects</h2>
       <div className="portfolio-section__gallery">
-        <div className="portfolio-section__gallery-item">1</div>
-        <div className="portfolio-section__gallery-item">2</div>
-        <div className="portfolio-section__gallery-item">3</div>
-        <div className="portfolio-section__gallery-item">4</div>
-        <div className="portfolio-section__gallery-item">5</div>
-        <div className="portfolio-section__gallery-item">6</div>
-        <div className="portfolio-section__gallery-item">7</div>
-        <div className="portfolio-section__gallery-item">8</div>
+        <Project className="portfolio-section__gallery-item" {...projectOne} />
       </div>
     </section>
   );
---
+++ src/Components/Project/data.js
@@ -0,0 +1,8 @@
+const projectOne = {
+  img: require("../../assets/images/desk.jpg"),
+  title: "Project number one",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s",
+};
+
+export { projectOne };
