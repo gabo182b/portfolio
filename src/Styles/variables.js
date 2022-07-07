e119b96344f05751a6db00b36bca19512304f5ad
gabriel <flexflit18@gmail.com>
2022-07-06 20:27:28 -0400

    SASS uninstalled

---
+++ src/Styles/variables.js
@@ -0,0 +1,11 @@
+export const fontSizes = {
+    sizeSm: '1rem',
+    sizeMd: '2rem',
+    sizeLg: '3.5rem',
+    sizeXl: '4.20rem,'
+}
+
+export const colors = {
+    backgroundColor: '#09010E',
+    white: '#FFFFFF'
+}
