40ff7635effa57565550265621df80527219a797
gabriel <flexflit18@gmail.com>
2022-07-27 12:31:25 -0400

    moved the get year method to a utilities folder

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { year } from "../utilities/getyear";
 import { FaTwitter, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import { FooterContainer, FooterList, FooterLink, Link, FooterTextContainer, FooterText } from "../../styled-components/FooterElements";
 
 const Footer = () => {
-  const year = new Date().getFullYear();
 
   return (
     <FooterContainer>
